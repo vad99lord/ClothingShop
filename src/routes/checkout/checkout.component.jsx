3b17aa95ebbf0c9d8f36a2c98d4699730f93fefe
Vadim <vad99lord@gmail.com>
2022-05-18 19:32:47 +0300

    update remaining scss to styled components

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,11 @@
 import { useContext } from "react";
 
-import "./checkout.style.scss";
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from "./checkout.styles";
 import { ProductCartContext } from "../../contexts/product-cart.context";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
@@ -8,96 +13,29 @@ const Checkout = () => {
   const { cartItems, cartTotal } = useContext(ProductCartContext);
 
   return (
-    <div className='checkout-container'>
-      <div className='checkout-header'>
-        <div className='header-block'>
+    <CheckoutContainer>
+      <CheckoutHeader>
+        <HeaderBlock>
           <span>Product</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Description</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Quantity</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Price</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Remove</span>
-        </div>
-      </div>
+        </HeaderBlock>
+      </CheckoutHeader>
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <div className='total'>TOTAL: ${cartTotal}</div>
-    </div>
+      <Total>TOTAL: ${cartTotal}</Total>
+    </CheckoutContainer>
   );
 };
-
-// const Checkout = () => {
-//   const {
-//     cartItems,
-//     addItemToCart,
-//     decreaseItemFromCart,
-//     removeItemFromCart,
-//     cartTotal
-//   } = useContext(ProductCartContext);
-//   return (
-//     <div>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Product</th>
-//             <th>Description</th>
-//             <th>Quantity</th>
-//             <th>Price</th>
-//             <th>Remove</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {cartItems.map((cartItem) => (
-//             <tr key={cartItem.id}>
-//               <td>
-//                 <img
-//                   src={cartItem.imageUrl}
-//                   alt={`${cartItem.name}`}
-//                 />
-//               </td>
-//               <td>{cartItem.name}</td>
-//               <td>
-//                 <Button
-//                   onClick={() => {
-//                     decreaseItemFromCart(cartItem);
-//                   }}
-//                 >
-//                   Decrease
-//                 </Button>
-//                 {cartItem.quantity}
-//                 <Button
-//                   onClick={() => {
-//                     addItemToCart(cartItem);
-//                   }}
-//                 >
-//                   Increase
-//                 </Button>
-//               </td>
-//               <td>{cartItem.price}</td>
-//               <td>
-//                 <Button
-//                   onClick={() => {
-//                     removeItemFromCart(cartItem);
-//                   }}
-//                 >
-//                   Remove
-//                 </Button>
-//               </td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       <h2>Total: {cartTotal}</h2>
-//     </div>
-//   );
-// };
-
 export default Checkout;
