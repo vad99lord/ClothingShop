e8923ff54ac3c77d8e57628167ee4fdcf6e36ff5
Vadim <vad99lord@gmail.com>
2022-05-19 13:33:40 +0300

    add redux dev tools support and cart persistance

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import App from "./App";
 
 import "./index.scss";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,35 @@ import {
   applyMiddleware,
 } from "redux";
 import logger from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
   undefined,
   composedEnhancers
 );
+
+export const persistor = persistStore(store);
