9d56778692020c3186377abe1ad2267aa28004b3
Vadim <vad99lord@gmail.com>
2022-05-20 19:34:06 +0300

    add stripe payment to checkout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import { Elements } from "@stripe/react-stripe-js";
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
-
-import "./index.scss";
 import { Provider } from "react-redux";
-import { persistor, store } from "./store/store";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import "./index.scss";
+import { persistor, store } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe";
 
 const rootElement = document.getElementById("root");
 
@@ -16,7 +16,9 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 import {
   selectCartItems,
   selectCartTotal,
@@ -38,6 +39,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>TOTAL: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
